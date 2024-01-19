1a3c5ac8f71fb14c50b07321eeb365f39bf19957
neon-sunset <neon_sunset@outlook.com>
2024-01-19 08:50:24 +0200

    ci: fix build

--- Benchmarks/Formatting.cs
+++ Benchmarks/Formatting.cs
@@ -13,37 +13,37 @@ public class Formatting
     const decimal Decimal = 42.42m;
 
     [Benchmark]
-    public U8String FormatDateTime() => U8String.Format($"Date is {DateTime}");
+    public U8String FormatDateTime() => new($"Date is {DateTime}");
 
     [Benchmark]
     public string FormatDateTimeU16() => $"Date is {DateTime}";
 
     [Benchmark]
-    public U8String FormatEnum() => U8String.Format($"Enum is ${StringComparison.Ordinal}");
+    public U8String FormatEnum() => new($"Enum is ${StringComparison.Ordinal}");
 
     [Benchmark]
     public string FormatEnumU16() => $"Enum is ${StringComparison.Ordinal}";
 
     [Benchmark]
-    public U8String FormatBool() => U8String.Format($"{true} {false}");
+    public U8String FormatBool() => new($"{true} {false}");
 
     [Benchmark]
     public string FormatBoolU16() => $"{true} {false}";
 
     [Benchmark]
-    public U8String FormatGuid() => U8String.Format($"Guid is {Guid}");
+    public U8String FormatGuid() => new($"Guid is {Guid}");
 
     [Benchmark]
     public string FormatGuidU16() => $"Guid is {Guid}";
 
     [Benchmark]
-    public U8String FormatDecimal() => U8String.Format($"Decimal is {Decimal}");
+    public U8String FormatDecimal() => new($"Decimal is {Decimal}");
 
     [Benchmark]
     public string FormatDecimalU16() => $"Decimal is {Decimal}";
 
     [Benchmark]
-    public U8String FormatMany() => U8String.Format($"{DateTime} {Guid} {Decimal}");
+    public U8String FormatMany() => new($"{DateTime} {Guid} {Decimal}");
 
     [Benchmark]
     public string FormatManyU16() => $"{DateTime} {Guid} {Decimal}";
